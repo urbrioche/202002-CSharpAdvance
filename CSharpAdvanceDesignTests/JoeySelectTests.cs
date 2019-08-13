60d5c2f9d3b11cd1ce649b72e9279fa53c354c2b
Joey Chen <jay.hatelove@gmail.com>
2019-08-13 17:25:38 +0800

    pass test: JoeySelectWithPort()

--- CSharpAdvanceDesignTests/JoeySelectTests.cs
+++ CSharpAdvanceDesignTests/JoeySelectTests.cs
@@ -63,7 +63,13 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<string> JoeySelectWithPort(IEnumerable<string> urls)
         {
-            throw new System.NotImplementedException();
+            var result = new List<string>();
+            foreach (var url in urls)
+            {
+                result.Add(url + ":9191");
+            }
+
+            return result;
         }
 
         private IEnumerable<string> JoeySelect(IEnumerable<string> urls)
