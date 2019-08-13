4566eb724928627b9d99b5633d01ac3504e71875
Joey Chen <jay.hatelove@gmail.com>
2019-08-13 16:26:02 +0800

    add a failed test: find index == 0  && index >1

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -101,5 +101,14 @@ namespace CSharpAdvanceDesignTests
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
+
+        [Test]
+        public void find_the_first_one_and_skip_second_one_and_take_others()
+        {
+            var numbers = new List<int> {1, 2, 3, 4, 5};
+            var joeyWhereWithIndex = JoeyWhereWithIndex(numbers);
+            var expected = new List<int> {1, 3, 4, 5};
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
     }
 }
