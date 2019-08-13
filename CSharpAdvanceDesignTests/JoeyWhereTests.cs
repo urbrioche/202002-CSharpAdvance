c5369379f9c2bf5a4507a3810680dcea6d942693
Joey Chen <jay.hatelove@gmail.com>
2019-08-13 18:34:12 +0800

    add test with Where().Select()

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -38,6 +38,34 @@ namespace CSharpAdvanceDesignTests
         }
 
         [Test]
+        public void find_price_more_than_700_and_select_id_and_price()
+        {
+            var products = new List<Product>
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
+                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
+                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
+            };
+
+            var actual = products
+                         .JoeyWhere(product => product.Price > 700)
+                         .JoeySelect(p => $"{p.Id}-{p.Price}");
+
+            var expected = new[]
+            {
+                "7-710",
+                "8-780",
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
         public void find_products_that_price_between_200_and_500_and_cost_less_than_30()
         {
             var products = new List<Product>
