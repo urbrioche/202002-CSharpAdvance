e2a571046b04bbae2be07e0a2c1d7540014152f3
Joey Chen <jay.hatelove@gmail.com>
2019-08-14 16:38:51 +0800

    add failed test for different skip while condition

--- CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
@@ -38,6 +38,37 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+        [Test]
+        public void skip_cards_when_point_less_than_5()
+        {
+            var cards = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 2},
+                new Card {Kind = CardKind.Normal, Point = 3},
+                new Card {Kind = CardKind.Normal, Point = 4},
+                new Card {Kind = CardKind.Separate},
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+
+            var actual = JoeySkipWhilePointLessThan5(cards);
+
+            var expected = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Card> JoeySkipWhilePointLessThan5(IEnumerable<Card> cards)
+        {
+            throw new NotImplementedException();
+        }
+
         private IEnumerable<Card> JoeySkipWhile(IEnumerable<Card> cards)
         {
             var enumerator = cards.GetEnumerator();
