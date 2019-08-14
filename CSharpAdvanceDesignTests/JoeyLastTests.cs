249fa9c0ac3c91ba8fe9032c13332424fc0359ba
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 02:45:28 +0800

    add failed test for JoeyLast()

---
+++ CSharpAdvanceDesignTests/JoeyLastTests.cs
@@ -0,0 +1,22 @@
+﻿using ExpectedObjects;
+using NUnit.Framework;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    public class JoeyLastTests
+    {
+        [Test]
+        public void get_last_number()
+        {
+            var numbers = new[] {1, 2, 3, 4};
+            var actual = JoeyLast(numbers);
+            4.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private int JoeyLast(int[] numbers)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
