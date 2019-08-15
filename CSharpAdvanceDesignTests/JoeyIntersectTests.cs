7bc961dc56bfa42209c67d8a7a134e321f60cda4
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 21:24:32 +0800

    add failed test: Intersect()

--- CSharpAdvanceDesignTests/JoeyIntersectTests.cs
+++ CSharpAdvanceDesignTests/JoeyIntersectTests.cs
@@ -5,18 +5,17 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyIntersectTests
     {
         [Test]
         public void intersect_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 7, 3 };
+            var first = new[] {1, 3, 5, 3};
+            var second = new[] {5, 7, 3, 7};
 
             var actual = JoeyIntersect(first, second);
 
-            var expected = new[] { 3, 5 };
+            var expected = new[] {3, 5};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
