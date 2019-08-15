51d233114ee71cfc3a77d6ef64178ca748cfc04c
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 20:33:59 +0800

    add failed test: Distinct()

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -6,16 +6,15 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyDistinctTests
     {
         [Test]
         public void distinct_numbers()
         {
-            var numbers = new[] { 91, 3, 91, -1 };
+            var numbers = new[] {91, 3, 91, -1};
             var actual = Distinct(numbers);
 
-            var expected = new[] { 91, 3, -1 };
+            var expected = new[] {91, 3, -1};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
