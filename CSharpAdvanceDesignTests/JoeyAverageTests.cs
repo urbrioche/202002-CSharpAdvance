2b40d1ebc2e299ff72b97b05e8b40ae6bdad2042
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 13:35:52 +0800

    complete JoeyAverage()

--- CSharpAdvanceDesignTests/JoeyAverageTests.cs
+++ CSharpAdvanceDesignTests/JoeyAverageTests.cs
@@ -11,16 +11,29 @@ namespace CSharpAdvanceDesignTests
         [Test]
         public void average_with_null_value()
         {
-            var numbers = new int?[] {2, 4, null, 6};
+            var numbers = new int?[] {1, 1, null, 4, 4};
 
             var actual = JoeyAverage(numbers);
 
-            4.ToExpectedObject().ShouldMatch(actual);
+            2.5d.ToExpectedObject().ShouldMatch(actual);
         }
 
         private double? JoeyAverage(IEnumerable<int?> numbers)
         {
-            throw new System.NotImplementedException();
+            var enumerator = numbers.GetEnumerator();
+            var result = 0;
+            var count = 0;
+            while (enumerator.MoveNext())
+            {
+                var number = enumerator.Current;
+                if (number.HasValue)
+                {
+                    result += number.Value;
+                    count++;
+                }
+            }
+
+            return result / (double) count;
         }
     }
 }
