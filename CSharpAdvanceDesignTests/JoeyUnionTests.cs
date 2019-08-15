cdabcf3209e1f18e1b6bcbe9d1c97771a5769ea8
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 21:14:34 +0800

    add failed test: Union()

--- CSharpAdvanceDesignTests/JoeyUnionTests.cs
+++ CSharpAdvanceDesignTests/JoeyUnionTests.cs
@@ -5,17 +5,16 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyUnionTests
     {
         [Test]
         public void union_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 3, 7 };
+            var first = new[] {1, 3, 5};
+            var second = new[] {5, 3, 7};
 
             var actual = JoeyUnion(first, second);
-            var expected = new[] { 1, 3, 5, 7 };
+            var expected = new[] {1, 3, 5, 7};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
