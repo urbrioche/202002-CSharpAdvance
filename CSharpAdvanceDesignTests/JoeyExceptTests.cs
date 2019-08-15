e355822659e25ea2bc6d183793dd0909e54c690f
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 21:21:24 +0800

    add failed test: Except()

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -5,17 +5,16 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyExceptTests
     {
         [Test]
         public void except_numbers()
         {
-            var first = new[] { 1, 3, 5, 7 };
-            var second = new[] { 7, 1, 4 };
+            var first = new[] {1, 3, 5, 7, 3};
+            var second = new[] {7, 1, 4, 1};
 
             var actual = JoeyExcept(first, second);
-            var expected = new[] { 3, 5 };
+            var expected = new[] {3, 5};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
