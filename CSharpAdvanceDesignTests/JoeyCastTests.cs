2e033b7b059590d97bd378e8e88ff3ec4400b588
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 19:52:49 +0800

    add failed test: Cast()

--- CSharpAdvanceDesignTests/JoeyCastTests.cs
+++ CSharpAdvanceDesignTests/JoeyCastTests.cs
@@ -7,13 +7,12 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyCastTests
     {
         [Test]
         public void cast_int_exception_when_ArrayList_has_string()
         {
-            var arrayList = new ArrayList { 1, "2", 3 };
+            var arrayList = new ArrayList {1, "2", 3};
 
             void TestDelegate() => JoeyCast<int>(arrayList);
 
