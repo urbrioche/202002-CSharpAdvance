9375324b48ed4099ab18c379e5a6cdea3c5efac8
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 13:30:14 +0800

    add failed test: JoeyAverage()

--- CSharpAdvanceDesignTests/JoeyAverageTests.cs
+++ CSharpAdvanceDesignTests/JoeyAverageTests.cs
@@ -5,13 +5,12 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyAverageTests
     {
         [Test]
         public void average_with_null_value()
         {
-            var numbers = new int?[] { 2, 4, null, 6 };
+            var numbers = new int?[] {2, 4, null, 6};
 
             var actual = JoeyAverage(numbers);
 
