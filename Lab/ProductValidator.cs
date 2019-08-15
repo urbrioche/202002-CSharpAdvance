e103d4aa8f3fe1bed63405180d4175f17b0ade7d
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 19:47:29 +0800

    add failed test: OfType()

--- Lab/ProductValidator.cs
+++ Lab/ProductValidator.cs
@@ -2,11 +2,18 @@
 
 namespace Lab
 {
-    public class ProductValidator : IValidator<Product>
+    public class ProfitValidator : IValidator<Product>
     {
         public bool Validate(Product model)
         {
             return model.Price - model.Cost >= 0;
+        }
+    }
+    public class ProductPriceValidator : IValidator<Product>
+    {
+        public bool Validate(Product model)
+        {
+            return model.Price > 0;
         }
     }
 }
