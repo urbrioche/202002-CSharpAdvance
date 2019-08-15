6299091a3efbfbd704ef464a762a4f5175f8afea
Joey Chen <jay.hatelove@gmail.com>
2019-08-15 19:58:19 +0800

    add failed test: Reverse()

--- CSharpAdvanceDesignTests/JoeyReverseTests.cs
+++ CSharpAdvanceDesignTests/JoeyReverseTests.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyReverseTests
     {
         [Test]
@@ -14,18 +13,18 @@ namespace CSharpAdvanceDesignTests
         {
             var employees = new List<Employee>
             {
-                new Employee(){FirstName = "Joey",LastName = "Chen"},
-                new Employee(){FirstName = "Tom",LastName = "Li"},
-                new Employee(){FirstName = "David",LastName = "Wang"},
+                new Employee() {FirstName = "Joey", LastName = "Chen"},
+                new Employee() {FirstName = "Tom", LastName = "Li"},
+                new Employee() {FirstName = "David", LastName = "Wang"},
             };
 
             var actual = JoeyReverse(employees);
 
             var expected = new List<Employee>
             {
-                new Employee(){FirstName = "David",LastName = "Wang"},
-                new Employee(){FirstName = "Tom",LastName = "Li"},
-                new Employee(){FirstName = "Joey",LastName = "Chen"},
+                new Employee() {FirstName = "David", LastName = "Wang"},
+                new Employee() {FirstName = "Tom", LastName = "Li"},
+                new Employee() {FirstName = "Joey", LastName = "Chen"},
             };
 
             expected.ToExpectedObject().ShouldMatch(actual);
