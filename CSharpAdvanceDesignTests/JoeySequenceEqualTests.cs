d723c766c0a2cb059a7c37665905e93eb036d0b1
Joey Chen <jay.hatelove@gmail.com>
2019-08-17 22:32:05 +0800

    add failed test: SequenceEqual()

--- CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
+++ CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
@@ -5,14 +5,13 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeySequenceEqualTests
     {
         [Test]
         public void compare_two_numbers_equal()
         {
-            var first = new List<int> { 3, 2, 1 };
-            var second = new List<int> { 3, 2, 1 };
+            var first = new List<int> {3, 2, 1};
+            var second = new List<int> {3, 2, 1};
 
             var actual = JoeySequenceEqual(first, second);
 
