c431d282974400f6f4600ea4f4700cd8cda302e0
Joey Chen <jay.hatelove@gmail.com>
2019-08-18 01:03:19 +0800

    safe delete parameter: "lookup"

--- CSharpAdvanceDesignTests/JoeyGroupByTests.cs
+++ CSharpAdvanceDesignTests/JoeyGroupByTests.cs
@@ -24,7 +24,7 @@ namespace CSharpAdvanceDesignTests
             }
         }
 
-        public IEnumerable<IGrouping<string, Employee>> ConvertToMyGrouping(Dictionary<string, List<Employee>> lookup)
+        public IEnumerable<IGrouping<string, Employee>> ConvertToMyGrouping()
         {
             var enumerator = _lookup.GetEnumerator();
             while (enumerator.MoveNext())
@@ -74,7 +74,7 @@ namespace CSharpAdvanceDesignTests
                 _myLookup.AddElement(employee);
             }
 
-            return _myLookup.ConvertToMyGrouping(new Dictionary<string, List<Employee>>());
+            return _myLookup.ConvertToMyGrouping();
         }
     }
 
