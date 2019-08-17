58fc5ca864b0ab5227b4054f5f4d527c1f1bbfdf
Joey Chen <jay.hatelove@gmail.com>
2019-08-18 01:04:50 +0800

    implement GetEnuemrator(0 by ConverToMyGrouping()

--- CSharpAdvanceDesignTests/JoeyGroupByTests.cs
+++ CSharpAdvanceDesignTests/JoeyGroupByTests.cs
@@ -12,6 +12,16 @@ namespace CSharpAdvanceDesignTests
     {
         private readonly Dictionary<string, List<Employee>> _lookup = new Dictionary<string, List<Employee>>();
 
+        public IEnumerator<IGrouping<string, Employee>> GetEnumerator()
+        {
+            return ConvertToMyGrouping();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
         public void AddElement(Employee employee)
         {
             if (_lookup.ContainsKey(employee.LastName))
@@ -24,7 +34,7 @@ namespace CSharpAdvanceDesignTests
             }
         }
 
-        public IEnumerable<IGrouping<string, Employee>> ConvertToMyGrouping()
+        public IEnumerator<IGrouping<string, Employee>> ConvertToMyGrouping()
         {
             var enumerator = _lookup.GetEnumerator();
             while (enumerator.MoveNext())
@@ -32,16 +42,6 @@ namespace CSharpAdvanceDesignTests
                 var keyValuePair = enumerator.Current;
                 yield return new MyGrouping(keyValuePair.Key, keyValuePair.Value);
             }
-        }
-
-        public IEnumerator<IGrouping<string, Employee>> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 
