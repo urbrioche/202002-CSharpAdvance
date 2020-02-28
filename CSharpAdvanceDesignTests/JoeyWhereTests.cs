7e5e6ecab03e0a007c63872cd3cfa8e6aea325e0
hatelove <jay.hatelove@gmail.com>
2020-02-28 15:42:48 +0800

    [test][where + select][iterator] example

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -102,5 +102,51 @@ namespace CSharpAdvanceDesignTests
             var expected = new List<int> {1, 3, 4};
             expected.ToExpectedObject().ShouldMatch(actual);
         }
+
+        [Test]
+        public void find_price_more_than_700_and_select_id_and_price()
+        {
+            var products = new List<Product>
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
+                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
+                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
+            };
+
+            var actual = products
+                .JoeyWhere(product => product.Price > 700)
+                .JoeySelect(p => $"{p.Id}-{p.Price}");
+
+            var joey = DelayExecute(products);
+
+            var expected = new[]
+            {
+                "7-710",
+                "8-780",
+            };
+
+            foreach (var item in actual)
+            {
+                Console.WriteLine(item);
+            }
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<string> DelayExecute(List<Product> products)
+        {
+            foreach (var product in products)
+            {
+                if (product.Price > 700)
+                {
+                    yield return $"{product.Id}-{product.Price}";
+                }
+            }
+        }
     }
 }
