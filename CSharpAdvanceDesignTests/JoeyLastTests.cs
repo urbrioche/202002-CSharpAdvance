1d632c4079ac7a954436cd4627b23c8d802d7342
jerry <aafulin@gmail.com>
2020-02-29 12:08:28 +0800

    JoeyLast

---
+++ CSharpAdvanceDesignTests/JoeyLastTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpectedObjects;
+using Lab.Entities;
+using NUnit.Framework;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    public class JoeyLastTests
+    {
+        [Test]
+        public void get_last_chen()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Cash", LastName = "Li"},
+            };
+
+            var employee = JoeyLast(employees);
+
+            new Employee { FirstName = "Cash", LastName = "Li" }
+                .ToExpectedObject().ShouldMatch(employee);
+        }
+
+        [Test]
+        public void get_last_employee_when_no_girls()
+        {
+            var employees = new Employee[]
+            {
+            };
+
+            TestDelegate action = () => JoeyLast(employees);
+            Assert.Throws<InvalidOperationException>(action);
+        }
+
+        private Employee JoeyLast(IEnumerable<Employee> employees)
+        {
+            var enumerator = employees.GetEnumerator();
+            if (!enumerator.MoveNext())
+            {
+                throw new InvalidOperationException($"{nameof(employees)} is empty");
+            }
+
+            var last = enumerator.Current;
+            while (enumerator.MoveNext())
+            {
+                last = enumerator.Current;
+            }
+
+            return last;
+        }
+    }
+}
