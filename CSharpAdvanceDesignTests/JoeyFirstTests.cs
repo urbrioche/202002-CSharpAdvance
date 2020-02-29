b8af7c53385b635fc8c985e71712ec2046ada4e2
jerry <aafulin@gmail.com>
2020-02-29 11:28:18 +0800

    JoeyFirstTests

--- CSharpAdvanceDesignTests/JoeyFirstTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstTests.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -6,7 +7,6 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyFirstTests
     {
         [Test]
@@ -14,20 +14,62 @@ namespace CSharpAdvanceDesignTests
         {
             var girls = new[]
             {
-                new Girl(){Age = 10},
+                new Girl(){Age = 60},
                 new Girl(){Age = 20},
                 new Girl(){Age = 30},
             };
 
             var girl = JoeyFirst(girls);
-            var expected = new Girl { Age = 10 };
+            var expected = new Girl { Age = 60 };
 
             expected.ToExpectedObject().ShouldEqual(girl);
         }
 
-        private Girl JoeyFirst(IEnumerable<Girl> girls)
+        [Test]
+        public void get_first_girl_when_no_girls()
         {
-            throw new System.NotImplementedException();
+            var girls = new Girl[]
+            {
+            };
+
+            TestDelegate action = () => JoeyFirst(girls);
+            Assert.Throws<InvalidOperationException>(action);
+        }
+
+        [Test]
+        public void get_first_chen()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"}
+            };
+            var employee = JoeyFirstWithCondition(employees, current => current.LastName == "Chen");
+            new Employee() { FirstName = "Joey", LastName = "Chen" }.ToExpectedObject().ShouldMatch(employee);
+        }
+
+        private TSource JoeyFirstWithCondition<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (predicate(current))
+                {
+                    return current;
+                }
+            }
+
+            throw new InvalidOperationException($"{nameof(source)} is empty");
+        }
+
+        private TSource JoeyFirst<TSource>(IEnumerable<TSource> source)
+        {
+            var enumerator = source.GetEnumerator();
+            return enumerator.MoveNext()
+                ? enumerator.Current
+                : throw new InvalidOperationException($"{nameof(source)} is empty");
         }
     }
 }
