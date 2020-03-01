411c044e63e9b7e0e7c43d6c5303a00b6438e5e0
hatelove <jay.hatelove@gmail.com>
2020-03-01 15:18:13 +0800

    [feat][intersect] pass

--- CSharpAdvanceDesignTests/JoeyIntersectTests.cs
+++ CSharpAdvanceDesignTests/JoeyIntersectTests.cs
@@ -5,25 +5,34 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyIntersectTests
     {
         [Test]
         public void intersect_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 7, 3 };
+            var first = new[] {1, 3, 5, 3};
+            var second = new[] {5, 7, 3, 7};
 
             var actual = JoeyIntersect(first, second);
 
-            var expected = new[] { 3, 5 };
+            var expected = new[] {3, 5};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<int> JoeyIntersect(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var secondHashSet = new HashSet<int>(second);
+            var enumerator = first.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+
+                if (secondHashSet.Remove(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
