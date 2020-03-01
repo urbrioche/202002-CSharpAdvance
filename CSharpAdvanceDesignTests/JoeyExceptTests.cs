7e154fe634e44309e39672e0f1b5e755dffe5f45
hatelove <jay.hatelove@gmail.com>
2020-03-01 15:28:06 +0800

    [test][except] add test for second except first

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -1,6 +1,7 @@
 ﻿using ExpectedObjects;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -10,11 +11,23 @@ namespace CSharpAdvanceDesignTests
         [Test]
         public void except_numbers()
         {
-            var first = new[] { 1, 3, 5, 7, 3 };
-            var second = new[] { 7, 1, 4, 1 };
+            var first = new[] {1, 3, 5, 7, 3};
+            var second = new[] {7, 1, 4, 1};
 
             var actual = JoeyExcept(first, second);
-            var expected = new[] { 3, 5 };
+            var expected = new[] {3, 5};
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void except_numbers_cash()
+        {
+            var first = new[] {1, 3, 5, 7, 3};
+            var second = new[] {7, 1, 4, 1};
+
+            var actual = JoeyExcept(second, first);
+            var expected = new[] {4};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
