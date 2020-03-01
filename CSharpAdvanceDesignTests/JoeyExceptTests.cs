9dadd6ade046eeda4d810f5791922c1c9db769d1
hatelove <jay.hatelove@gmail.com>
2020-03-01 15:44:14 +0800

    [anti-pattern] don't use first.Where(f=>second.Contains(f));

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -11,11 +11,14 @@ namespace CSharpAdvanceDesignTests
         [Test]
         public void except_numbers()
         {
-            var first = new[] {1, 3, 5, 7, 3};
-            var second = new[] {7, 1, 4, 1};
+            var first = new[] { 1, 3, 5, 7 };
+            var second = new[] { 7, 1, 4, 1 };
 
-            var actual = JoeyExcept(first, second);
-            var expected = new[] {3, 5};
+            //var actual = JoeyExcept(first, second);
+            //var actual = new List<int>();
+            var actual = first.Where(x => !second.Contains(x));
+
+            var expected = new[] { 3, 5 };
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
@@ -23,11 +26,12 @@ namespace CSharpAdvanceDesignTests
         [Test]
         public void except_numbers_cash()
         {
-            var first = new[] {1, 3, 5, 7, 3};
-            var second = new[] {7, 1, 4, 1};
+            var first = new[] { 1, 3, 5, 7, 3 };
+            var second = new[] { 7, 1, 4, 1 };
 
             var actual = JoeyExcept(second, first);
-            var expected = new[] {4};
+
+            var expected = new[] { 4 };
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
@@ -43,7 +47,7 @@ namespace CSharpAdvanceDesignTests
                 {
                     yield return current;
                 }
-            } 
+            }
         }
     }
 }
