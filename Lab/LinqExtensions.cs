d9328a49ff3772bd491c533b471c35f918dd86af
hatelove <jay.hatelove@gmail.com>
2020-03-01 10:16:09 +0800

    [refactor][orderBy]extract class MyOrderedEnumerable from JoeySort()

--- Lab/LinqExtensions.cs
+++ Lab/LinqExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Lab.Entities;
 
 namespace Lab
@@ -164,32 +163,6 @@ namespace Lab
             Func<Employee, TKey> keySelector)
         {
             return employees;
-        }
-
-        public static IEnumerable<Employee> JoeySort(this IEnumerable<Employee> employees,
-            IComparer<Employee> comboComparer)
-        {
-            //selection sort
-            var elements = employees.ToList();
-
-            while (elements.Any())
-            {
-                var minElement = elements[0];
-                var index = 0;
-                for (int i = 1; i < elements.Count; i++)
-                {
-                    var employee = elements[i];
-
-                    if (comboComparer.Compare(employee, minElement) < 0)
-                    {
-                        minElement = employee;
-                        index = i;
-                    }
-                }
-
-                elements.RemoveAt(index);
-                yield return minElement;
-            }
         }
     }
 }
