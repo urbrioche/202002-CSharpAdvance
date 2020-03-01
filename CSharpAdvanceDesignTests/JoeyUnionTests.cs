77a880c6adea932e37c59df39d7ffe660ca8560b
hatelove <jay.hatelove@gmail.com>
2020-03-01 14:50:58 +0800

    [feat][union] pass

--- CSharpAdvanceDesignTests/JoeyUnionTests.cs
+++ CSharpAdvanceDesignTests/JoeyUnionTests.cs
@@ -5,24 +5,52 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyUnionTests
     {
         [Test]
         public void union_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 3, 7 };
+            var first = new[] {1, 3, 5, 3};
+            var second = new[] {5, 3, 7};
 
             var actual = JoeyUnion(first, second);
-            var expected = new[] { 1, 3, 5, 7 };
+            var expected = new[] {1, 3, 5, 7};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<int> JoeyUnion(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var hashSet = new HashSet<int>();
+            var firstEnumerator = first.GetEnumerator();
+            while (firstEnumerator.MoveNext())
+            {
+                var current = firstEnumerator.Current;
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+            }
+
+            var secondEnumerator = second.GetEnumerator();
+            while (secondEnumerator.MoveNext())
+            {
+                var current = secondEnumerator.Current;
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+            } 
+
+            //var hashSet = new HashSet<int>(first);
+            //var enumerator = second.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    var current = enumerator.Current;
+            //    hashSet.Add(current);
+            //}
+
+            //return hashSet; 
         }
     }
 }
