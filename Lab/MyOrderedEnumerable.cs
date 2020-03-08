b567a4d05ea275e3825ac57d0b834b075b6640f6
jerry <aafulin@gmail.com>
2020-03-08 14:44:08 +0800

    copy JoeySort method to MyOrderedEnumable class, and Implement GetEnumerator to call JoeySort method, but compile error due to different type, alt+enter to change JoeySort type

--- Lab/MyOrderedEnumerable.cs
+++ Lab/MyOrderedEnumerable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Lab.Entities;
 
 namespace Lab
@@ -18,12 +19,38 @@ namespace Lab
 
         public IEnumerator<Employee> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return JoeySort(_employees, _combineKeyComparer);
+            //throw new NotImplementedException();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
+
+        private IEnumerator<Employee> JoeySort(IEnumerable<Employee> employees, IComparer<Employee> comboComparer)
+        {
+            //selection sort
+            var elements = employees.ToList();
+            while (elements.Any())
+            {
+                var minElement = elements[0];
+                var index = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    var employee = elements[i];
+
+                    if (comboComparer.Compare(employee, minElement) < 0)
+                    {
+                        minElement = employee;
+                        index = i;
+                    }
+                }
+
+                elements.RemoveAt(index);
+                yield return minElement;
+            }
+        }
+
     }
 }
