226072e856dc1e7bceea114c815bb027891570dd
jerry <aafulin@gmail.com>
2020-03-08 14:36:35 +0800

    implement IEnumerable<Employee> to make compile successful, still tet fail

--- Lab/MyOrderedEnumerable.cs
+++ Lab/MyOrderedEnumerable.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Lab.Entities;
 
 namespace Lab
 {
-    public class MyOrderedEnumerable
+    public class MyOrderedEnumerable : IEnumerable<Employee>
     {
         private readonly IEnumerable<Employee> _employees;
         private readonly IComparer<Employee> _combineKeyComparer;
@@ -13,6 +14,16 @@ namespace Lab
         {
             _employees = employees;
             _combineKeyComparer = combineKeyComparer;
+        }
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
