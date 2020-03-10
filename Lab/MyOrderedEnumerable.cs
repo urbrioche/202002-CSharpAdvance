53f7c7f9a9e8e49d28e19db26464fe6daebb5cf8
jerry <aafulin@gmail.com>
2020-03-10 20:09:03 +0800

    Implement GetEnumerator() method and pass test

--- Lab/MyOrderedEnumerable.cs
+++ Lab/MyOrderedEnumerable.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Lab.Entities;
 
 namespace Lab
 {
     public class MyOrderedEnumerable:IEnumerable<Employee>
     {
+        private readonly IEnumerable<Employee> _employees;
         private IComparer<Employee> _untilNowComparer;
 
         public MyOrderedEnumerable(IEnumerable<Employee> employees, IComparer<Employee> untilNowComparer)
         {
+            _employees = employees;
             _untilNowComparer = untilNowComparer;
         }
 
         public IEnumerator<Employee> GetEnumerator()
         {
-            throw new NotImplementedException();
+            //selection sort
+            var elements = _employees.ToList();
+            while (elements.Any())
+            {
+                var minElement = elements[0];
+                var index = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    if (_untilNowComparer.Compare(elements[i], minElement) < 0)
+                    {
+                        minElement = elements[i];
+                        index = i;
+                    }
+                }
+
+                elements.RemoveAt(index);
+                yield return minElement;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
