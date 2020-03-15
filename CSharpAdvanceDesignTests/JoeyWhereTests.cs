01b0da08b136d356945ce7b7b0ad45e44fd75a25
jerry <aafulin@gmail.com>
2020-03-15 18:53:12 +0800

    [refactor][where] extract parameter "predicate" for "JoeyWhereCostLessThan30"

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -51,7 +51,7 @@ namespace CSharpAdvanceDesignTests
                 new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
             };
 
-            var actual = JoeyWhereCostLessThan30(products);
+            var actual = JoeyWhereCostLessThan30(products, product => product.Price > 200 && product.Price < 500 && product.Cost < 30);
 
             var expected = new List<Product>
             {
@@ -61,12 +61,12 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private List<Product> JoeyWhereCostLessThan30(List<Product> products)
+        private List<Product> JoeyWhereCostLessThan30(List<Product> products, Func<Product, bool> predicate)
         {
             var result = new List<Product>();
             foreach (var product in products)
             {
-                if (product.Price > 200 && product.Price < 500 && product.Cost < 30)
+                if (predicate(product))
                 {
                     result.Add(product);
                 }
