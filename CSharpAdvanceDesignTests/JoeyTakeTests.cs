0571583c4147e24f7a374db8266dd8379ee0e54d
jerry <aafulin@gmail.com>
2020-03-17 19:59:21 +0800

    [feat][take] complete JoeyTake

--- CSharpAdvanceDesignTests/JoeyTakeTests.cs
+++ CSharpAdvanceDesignTests/JoeyTakeTests.cs
@@ -3,17 +3,24 @@ using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyTakeTests
     {
         [Test]
         public void take_2_employees()
         {
-            var employees = GetEmployees();
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Mike", LastName = "Chang"},
+                new Employee {FirstName = "Joseph", LastName = "Yao"},
+            };
 
             var actual = JoeyTake(employees);
 
@@ -23,12 +30,27 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Tom", LastName = "Li"},
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
+            expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees)
         {
-            throw new System.NotImplementedException();
+            var enumerator = employees.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (index < 2)
+                {
+                    yield return current;
+                }
+                else
+                {
+                    yield break;
+                }
+
+                index++;
+            }
         }
 
         private static IEnumerable<Employee> GetEmployees()
