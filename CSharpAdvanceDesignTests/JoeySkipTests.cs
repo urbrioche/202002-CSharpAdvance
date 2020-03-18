adc7ffd1d691ab1144011663ce22ac592a351961
jerry <aafulin@gmail.com>
2020-03-18 19:58:34 +0800

    [feat][skip] complete JoeySkip

--- CSharpAdvanceDesignTests/JoeySkipTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipTests.cs
@@ -7,15 +7,21 @@ using System.Linq;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeySkipTests
     {
         [Test]
         public void skip_2_employees()
         {
-            var employees = GetEmployees();
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Mike", LastName = "Chang"},
+                new Employee {FirstName = "Joseph", LastName = "Yao"},
+            };
 
-            var actual = JoeySelect(employees);
+            var actual = JoeySkip(employees, 2);
 
             var expected = new List<Employee>
             {
@@ -24,12 +30,22 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joseph", LastName = "Yao"},
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+            expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Employee> JoeySelect(IEnumerable<Employee> employees)
+        private IEnumerable<Employee> JoeySkip(IEnumerable<Employee> employees, int count)
         {
-            throw new System.NotImplementedException();
+            var enumerator = employees.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (index >= count)
+                {
+                    yield return current;
+                }
+                index++;
+            }
         }
 
         private static IEnumerable<Employee> GetEmployees()
