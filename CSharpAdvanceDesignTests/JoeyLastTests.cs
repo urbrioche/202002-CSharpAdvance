6b0a795fe4599c759ef3b4f9bf8a8b85e77f79ff
jerry <aafulin@gmail.com>
2020-03-21 18:10:33 +0800

    [test][last] add test for Last

---
+++ CSharpAdvanceDesignTests/JoeyLastTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ExpectedObjects;
+using Lab.Entities;
+using NUnit.Framework;
+
+namespace CSharpAdvanceDesignTests
+{
+    [TestFixture]
+    public class JoeyLastTests
+    {
+        [Test]
+        public void get_last_chen()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Cash", LastName = "Li"},
+            };
+
+            var employee = JoeyLast(employees);
+
+            new Employee { FirstName = "Cash", LastName = "Li" }
+                .ToExpectedObject().ShouldMatch(employee);
+        }
+
+        [Test]
+        public void get_last_employee_when_no_employees()
+        {
+            var employees = new Employee[]
+            {
+            };
+
+            TestDelegate action = () => JoeyLast(employees);
+            Assert.Throws<InvalidOperationException>(action);
+        }
+        
+        private Employee JoeyLast(ICollection<Employee> employees)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
