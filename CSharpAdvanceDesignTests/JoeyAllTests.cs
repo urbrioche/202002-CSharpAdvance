d29bbd0e9795c931e0159127350ac47b9225dd42
jerry <aafulin@gmail.com>
2020-03-21 15:47:26 +0800

    [refactor][all] extract parameter predicate

--- CSharpAdvanceDesignTests/JoeyAllTests.cs
+++ CSharpAdvanceDesignTests/JoeyAllTests.cs
@@ -1,4 +1,5 @@
-﻿using Lab.Entities;
+﻿using System;
+using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
 
@@ -19,7 +20,7 @@ namespace CSharpAdvanceDesignTests
                 new Girl{Age = 30},
             };
 
-            var actual = JoeyAll(girls);
+            var actual = JoeyAll(girls, girl => girl.Age >=18);
             Assert.IsFalse(actual);
         }
 
@@ -35,17 +36,17 @@ namespace CSharpAdvanceDesignTests
                 new Girl{Age = 30},
             };
 
-            var actual = JoeyAll(girls);
+            var actual = JoeyAll(girls, girl => girl.Age >=18);
             Assert.IsTrue(actual);
         }
 
-        private bool JoeyAll(IEnumerable<Girl> girls)
+        private bool JoeyAll(IEnumerable<Girl> girls, Func<Girl, bool> predicate)
         {
             var enumerator = girls.GetEnumerator();
             while (enumerator.MoveNext())
             {
                 var girl = enumerator.Current;
-                if (girl.Age >=18)
+                if (predicate(girl))
                 {
                     continue;
                 }
