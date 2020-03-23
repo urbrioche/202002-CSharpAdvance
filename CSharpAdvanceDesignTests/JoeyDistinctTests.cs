6954745176f11e5e116b560bf1327181bcf3bdf7
jerry <aafulin@gmail.com>
2020-03-23 19:15:48 +0800

    [test][distinct] distinct Employees

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using Lab.Entities;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -19,9 +20,50 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+        [Test]
+        public void distinct_employees()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyDistinct(employees);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Employee> JoeyDistinct(IEnumerable<Employee> employees)
+        {
+            return new HashSet<Employee>(employees, new FullNameEqualityComparer());
+        }
+
         private IEnumerable<int> Distinct(IEnumerable<int> numbers)
         {
             return new HashSet<int>(numbers);
+        }
+    }
+
+    internal class FullNameEqualityComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return x.FirstName == y.FirstName && x.LastName == y.LastName;
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            return new {obj.FirstName, obj.LastName}.GetHashCode();
         }
     }
 }
