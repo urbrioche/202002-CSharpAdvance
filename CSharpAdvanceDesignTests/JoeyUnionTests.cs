729bbfabe457f704ce934ece14689e2c607d70c2
jerry <aafulin@gmail.com>
2020-03-23 20:43:06 +0800

    [feat][union] pass tests

--- CSharpAdvanceDesignTests/JoeyUnionTests.cs
+++ CSharpAdvanceDesignTests/JoeyUnionTests.cs
@@ -5,13 +5,12 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyUnionTests
     {
         [Test]
         public void union_numbers()
         {
-            var first = new[] { 1, 3, 5 };
+            var first = new[] { 1, 3, 5, 3 };
             var second = new[] { 5, 3, 7 };
 
             var actual = JoeyUnion(first, second);
@@ -22,7 +21,28 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyUnion(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var hashSet = new HashSet<int>();
+            var firstEnumerator = first.GetEnumerator();
+            while (firstEnumerator.MoveNext())
+            {
+                var current = firstEnumerator.Current;
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+                
+            }
+
+            var secondEnumerator = second.GetEnumerator();
+            while (secondEnumerator.MoveNext())
+            {
+                var current = secondEnumerator.Current;
+
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
