169ca1f692489242aa5c5c18957c1a78252aed2d
jerry <aafulin@gmail.com>
2020-03-23 21:06:01 +0800

    [feat][except] pass tests

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -5,14 +5,13 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyExceptTests
     {
         [Test]
         public void except_numbers()
         {
-            var first = new[] { 1, 3, 5, 7 };
-            var second = new[] { 7, 1, 4 };
+            var first = new[] { 1, 3, 5, 7, 3 };
+            var second = new[] { 7, 1, 4, 1};
 
             var actual = JoeyExcept(first, second);
             var expected = new[] { 3, 5 };
@@ -22,7 +21,17 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyExcept(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var hashSet = new HashSet<int>(second);
+            var enumerator = first.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+
+                if (hashSet.Add(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
