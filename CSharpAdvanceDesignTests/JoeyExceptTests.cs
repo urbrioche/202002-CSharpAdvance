3fea494f3e5fd7b21a094f6a17f667a8770cc859
jerry <aafulin@gmail.com>
2020-03-23 21:08:32 +0800

    [test][except] add test case for second except first and pass the test

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -11,10 +11,22 @@ namespace CSharpAdvanceDesignTests
         public void except_numbers()
         {
             var first = new[] { 1, 3, 5, 7, 3 };
-            var second = new[] { 7, 1, 4, 1};
+            var second = new[] { 7, 1, 4, 1 };
 
             var actual = JoeyExcept(first, second);
             var expected = new[] { 3, 5 };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void except_numbers_second_except_first()
+        {
+            var first = new[] { 1, 3, 5, 7, 3 };
+            var second = new[] { 7, 1, 4, 1 };
+
+            var actual = JoeyExcept(second, first);
+            var expected = new[] { 4 };
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
