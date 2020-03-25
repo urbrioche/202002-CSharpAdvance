b7696d62d86197c35f1d5b9a2734cfa1a9d5293a
jerry <aafulin@gmail.com>
2020-03-25 21:51:50 +0800

    [feat][skipLast] pass tests

--- CSharpAdvanceDesignTests/JoeySkipLastTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipLastTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ExpectedObjects;
 using NUnit.Framework;
 
@@ -19,9 +20,54 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+
+        [Test]
+        public void skip_last_2_with_2_numbers()
+        {
+            var numbers = new[] { 40, 50 };
+            var actual = JoeySkipLast(numbers, 2);
+
+            var expected = new int[]{ };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void skip_last_2_with_1_numbers()
+        {
+            var numbers = new[] { 40 };
+            var actual = JoeySkipLast(numbers, 2);
+
+            var expected = new int[]{ };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
         private IEnumerable<int> JoeySkipLast(IEnumerable<int> numbers, int count)
         {
-            throw new NotImplementedException();
+            var queue = new Queue<int>();
+            var enumerator = numbers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (queue.Count == count)
+                {
+                    yield return queue.Dequeue();
+                }
+
+                queue.Enqueue(current);
+            }
+
+            //var queue = new Queue<int>(numbers);
+
+            //var enumerator = numbers.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    if (queue.Count > count)
+            //    {
+            //        yield return queue.Dequeue();
+            //    }
+            //}
         }
     }
 }
