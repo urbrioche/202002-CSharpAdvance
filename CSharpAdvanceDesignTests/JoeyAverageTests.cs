3155cc5fb3a64dadc154bc354c9420c45b6047ca
jerry <aafulin@gmail.com>
2020-04-17 20:28:20 +0800

    [test][average] add fail test

--- CSharpAdvanceDesignTests/JoeyAverageTests.cs
+++ CSharpAdvanceDesignTests/JoeyAverageTests.cs
@@ -1,11 +1,11 @@
 ﻿using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using ExpectedObjects;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyAverageTests
     {
         [Test]
@@ -15,7 +15,7 @@ namespace CSharpAdvanceDesignTests
 
             var actual = JoeyAverage(numbers);
 
-            //var expected = ?
+            4.ToExpectedObject().ShouldMatch(actual);
         }
 
         private double? JoeyAverage(IEnumerable<int?> numbers)
