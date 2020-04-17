71377fdf20fae044c2a8010200d4320db2cf4c52
jerry <aafulin@gmail.com>
2020-04-17 20:45:06 +0800

    [test][average] result with decimal place, test fail

--- CSharpAdvanceDesignTests/JoeyAverageTests.cs
+++ CSharpAdvanceDesignTests/JoeyAverageTests.cs
@@ -19,6 +19,16 @@ namespace CSharpAdvanceDesignTests
             4d.ToExpectedObject().ShouldMatch(actual);
         }
 
+        [Test]
+        public void average_result_with_decimal_places_source_with_null_value()
+        {
+            var numbers = new int?[] { 1, 1, null, 4, 4 };
+
+            var actual = JoeyAverage(numbers);
+
+            2.5d.ToExpectedObject().ShouldMatch(actual);
+        }
+
         private double? JoeyAverage(IEnumerable<int?> numbers)
         {
             var enumerator = numbers.GetEnumerator();
