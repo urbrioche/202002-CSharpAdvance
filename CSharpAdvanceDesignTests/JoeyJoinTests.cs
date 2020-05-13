7f7a0b3f33d867ce68a176a402e315ce2066dac7
jerry <aafulin@gmail.com>
2020-05-13 20:08:30 +0800

    [refactor][Join] extract outerKeySelector, innerKeySelector and resultSelector

--- CSharpAdvanceDesignTests/JoeyJoinTests.cs
+++ CSharpAdvanceDesignTests/JoeyJoinTests.cs
@@ -31,7 +31,7 @@ namespace CSharpAdvanceDesignTests
                 new Pet() {Name = "QQ", Owner = joey},
             };
 
-            var actual = JoeyJoin(employees, pets);
+            var actual = JoeyJoin(employees, pets, employee => employee, pet => pet.Owner, (employee1, pet1) => Tuple.Create(employee1.FirstName, pet1.Name));
 
             var expected = new[]
             {
@@ -44,7 +44,11 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Tuple<string, string>> JoeyJoin(IEnumerable<Employee> employees, IEnumerable<Pet> pets)
+        private IEnumerable<Tuple<string, string>> JoeyJoin(
+            IEnumerable<Employee> employees,
+            IEnumerable<Pet> pets,
+            Func<Employee, Employee> outerKeySelector,
+            Func<Pet, Employee> innerKeySelector, Func<Employee, Pet, Tuple<string, string>> resultSelector)
         {
             var employeeEnumerator = employees.GetEnumerator();
             // var comparer = EqualityComparer<Employee>.Default;
@@ -56,9 +60,9 @@ namespace CSharpAdvanceDesignTests
                 while (petEnumerator.MoveNext())
                 {
                     var pet = petEnumerator.Current;
-                    if (comparer.Equals(employee, pet.Owner))
+                    if (comparer.Equals(outerKeySelector(employee), innerKeySelector(pet)))
                     {
-                        yield return Tuple.Create(employee.FirstName, pet.Name);
+                        yield return resultSelector(employee, pet);
                     }
                 }
             }
